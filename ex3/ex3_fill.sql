300c4ab93ffb25fe2f316e6f2888a094b2848f95
Taiyrbek Anarbekov <an.taku2455@gmail.com>
2025-07-04 00:40:46 +0600

    add filling

---
+++ ex3/ex3_fill.sql
@@ -0,0 +1,13 @@
+INSERT INTO products (name) VALUES
+('Молоко'),
+('Сахар'),
+('Соль');
+
+INSERT INTO product_prices (product_id, price, date) VALUES
+(1, 90, '2024-07-01'),
+(2, 120, '2024-07-03'),
+(3, 40, '2024-07-03');
+
+
+INSERT INTO product_prices (product_id, price, date) VALUES
+(1, 9034, '2024-08-02');
